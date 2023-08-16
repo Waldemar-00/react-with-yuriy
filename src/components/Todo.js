ea9ab02710bc100faac3cb39b49c2d34571ad204
uladzimir <01myit@gmail.com>
2023-08-16 20:59:51 +0300

    refractoring function Todo, less functions

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,26 +4,23 @@ import Backdrop from './Backdrop';
 import Modal from './Modal';
 
 function Todo(props) {
-  const [showModal, setShowModal] = useState();
+  const [showModal, setShowModal] = useState(false);
 
-  function showModalHandler() {
-    setShowModal(true);
+  function modalHandler() {
+    setShowModal(!showModal);
   }
 
-  function closeModalHandler() {
-    setShowModal(false);
-  }
 
   return (
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
-        <button className='btn' onClick={showModalHandler}>
+        <button className='btn' onClick={modalHandler}>
           Удалить
         </button>
       </div>
-      {showModal && <Backdrop onClick={closeModalHandler} />}
-      {showModal && <Modal text='Вы уверены?' onClose={closeModalHandler} />}
+      {showModal ?  <Backdrop onClick={modalHandler} /> : null}
+      {showModal ?  <Modal text='Вы уверены?' onClose={modalHandler} /> : null}
     </div>
   );
 }
